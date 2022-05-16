56ef6692f697104a98a5c887890fd5baaa3cf7a0
Jake Miller <j_miller@ucsb.edu>
2022-05-15 23:26:50 -0700

    jm - added request for permission to get location and a static marker that shows position of the device when the map is initialized

--- app/src/main/java/edu/ucsb/cs/cs184/j_miller/h2go/MapsActivity.kt
+++ app/src/main/java/edu/ucsb/cs/cs184/j_miller/h2go/MapsActivity.kt
@@ -1,19 +1,37 @@
 package edu.ucsb.cs.cs184.j_miller.h2go
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import edu.ucsb.cs.cs184.j_miller.h2go.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var mLocProvider: FusedLocationProviderClient
+    private var mLocation: LatLng? = null
+    private var mLocMarker: Marker? = null
+
+    private var locationPermissionGranted = false
+    private val LOCATION_REQUEST_CODE = 101
     private lateinit var binding: ActivityMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,10 +40,77 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Ask user for permission to use location data
+        setupPermissions()
+
+        if (locationPermissionGranted) {
+            mLocProvider = LocationServices.getFusedLocationProviderClient(this)
+        }
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+
+    }
+
+    private fun setupPermissions() {
+        val locationPermission = ContextCompat.checkSelfPermission(
+            this, Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (locationPermission != PackageManager.PERMISSION_GRANTED) {
+            Log.i("MapsActivity", "Permission denied")
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                val builder = AlertDialog.Builder(this)
+                builder.setMessage("Location permission is required to show your location on the map. " +
+                        "You can still view the map without it.")
+                    .setTitle("Permission required")
+                builder.setPositiveButton("OK") { dialog, id ->
+                    Log.i("MapsActivity", "Clicked")
+                    ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        LOCATION_REQUEST_CODE)
+                }
+                val dialog = builder.create()
+                dialog.show()
+            } else {
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    LOCATION_REQUEST_CODE)
+            }
+        } else {
+            locationPermissionGranted = true
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_REQUEST_CODE -> {
+                locationPermissionGranted =
+                    !(grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED)
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+        if (locationPermissionGranted) {
+            val newLocation = mLocProvider.lastLocation
+            newLocation.addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    mLocation = LatLng(task.result.latitude, task.result.longitude)
+                    if (mLocation != null) {
+                        mLocMarker = mMap.addMarker(
+                            MarkerOptions().position(mLocation!!).title("You are Here")
+                        )
+                    }
+                }
+            }
+        }
     }
 
     /**
@@ -44,5 +129,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val ucenBottleRefill = LatLng(34.41151074066825, -119.84841258570862)
         mMap.addMarker(MarkerOptions().position(ucenBottleRefill).title("UCEN - Water Bottle Refill"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(ucenBottleRefill))
+
+        if (locationPermissionGranted) {
+            getLocation()
+        }
     }
 }
