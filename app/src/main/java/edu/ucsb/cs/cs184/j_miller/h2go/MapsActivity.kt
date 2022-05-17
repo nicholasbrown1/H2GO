112d0bd064d5aab5aff23457583f88e1c6134b9e
Nicholas Brown <countdooku.steak@gmail.com>
2022-05-17 14:38:26 -0700

    nb - used icon from show-location branch

--- app/src/main/java/edu/ucsb/cs/cs184/j_miller/h2go/MapsActivity.kt
+++ app/src/main/java/edu/ucsb/cs/cs184/j_miller/h2go/MapsActivity.kt
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.firestore.ktx.firestore
@@ -49,7 +50,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .addOnSuccessListener { result ->
                 for (location in result) {
                     ucenBottleRefill = LatLng(location.data["lat"] as Double, location.data["long"] as Double)
-                    mMap.addMarker(MarkerOptions().position(ucenBottleRefill).title(location.data["title"] as String))
+                    mMap.addMarker(MarkerOptions()
+                        .position(ucenBottleRefill)
+                        .title(location.data["title"] as String)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)))
                     if (location.id == "primary_location") {
                         mMap.moveCamera(CameraUpdateFactory.newLatLng(ucenBottleRefill))
                     }
