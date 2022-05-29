749d7f3d8e0adc47f7b2055dcf2516f53d80b3e4
Ethan Meade <ethanmeade@ucsb.edu>
2022-05-29 16:21:35 -0700

    EM - Created MapsViewModel

---
+++ app/src/main/java/edu/ucsb/cs/cs184/j_miller/h2go/MapsViewModel.kt
@@ -0,0 +1,12 @@
+package edu.ucsb.cs.cs184.j_miller.h2go
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MapsViewModel: ViewModel() {
+
+
+
+}
